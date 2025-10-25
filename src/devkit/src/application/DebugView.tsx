6ea04b78042c9d666b583ce9b7421d142199053f
James Randall <james@accidentalfish.com>
2025-10-25 09:45:44 +0100

    Split view in place

---
+++ src/devkit/src/application/DebugView.tsx
@@ -0,0 +1,5 @@
+export function DebugView() {
+    return <div className="h-full w-full bg-zinc-800">
+        DebugView
+    </div>
+}
---
+++ src/devkit/src/application/Editor.tsx
@@ -0,0 +1,5 @@
+export function Editor() {
+    return <div className="h-full w-full bg-zinc-800">
+        Editor
+    </div>
+}
